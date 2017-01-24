dc6243aef98e1acc509aa0fe4284cba5d0d8ae00
Andy Ong <aong93@gmail.com>
2017-01-24 15:07:43 -0800

    Level 0 - math combo

---
+++ practice31/script.js
@@ -0,0 +1,9 @@
+function cocococombomath(num1, num2, num3, num4, num5){
+    var result = num1 * num2;
+    result += num3;
+    result /= num4;
+    result -= num5;
+    return result;
+}
+
+console.log(cocococombomath(3,2,6,3,5));
