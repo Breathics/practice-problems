54098e230a50012db0ecdeb2bc7c249e32a8227f
Andy Ong <aong93@gmail.com>
2017-02-12 21:12:19 -0800

    Fixed syntax error to initialize for loop

--- practice21/answer.js
+++ practice21/answer.js
@@ -1,6 +1,6 @@
 // answer.js
 function bipolarLoop(){
-  for (var i = 0; var j = 10; i < 10; j > 0; i++; j--){
+  for (var i = 0, j = 10; i < 10, j > 0; i++, j--){
     console.log('This is i:',i);
     console.log('This is j:',j);
   }
