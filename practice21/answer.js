6ea35a51b28bddbe92189ce211419707c040459c
Andy Ong <aong93@gmail.com>
2017-02-12 21:08:30 -0800

    Created for loop, will not run through jsbin

--- practice21/answer.js
+++ practice21/answer.js
@@ -1,1 +1,9 @@
-answer.js
+// answer.js
+function bipolarLoop(){
+  for (var i = 0; var j = 10; i < 10; j > 0; i++; j--){
+    console.log('This is i:',i);
+    console.log('This is j:',j);
+  }
+}
+
+bipolarLoop();
