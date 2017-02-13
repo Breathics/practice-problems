55fc4077d18570513169ee292e20dcb8d1823d9b
Andy Ong <aong93@gmail.com>
2017-02-12 23:33:54 -0800

    Reset master

--- practice21/answer.js
+++
@@ -1,9 +0,0 @@
-// answer.js
-function bipolarLoop(){
-  for (var i = 0, j = 10; i < 10, j > 0; i++, j--){
-    console.log('This is i:',i);
-    console.log('This is j:',j);
-  }
-}
-
-bipolarLoop();
