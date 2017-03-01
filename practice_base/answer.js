0d8f1f5f466e7eabe0ba5868ee738a6d9acbf549
Andy Ong <aong93@gmail.com>
2017-03-01 12:07:17 -0800

    working on 3rd for loop

--- practice_base/answer.js
+++ practice_base/answer.js
@@ -1,27 +1,33 @@
 var a = '1375892';
-var b = a.split('');
+var b = a.split('').map(Number);
 var holdTen = [];
 
-function toTen(strNum){
-  for (var i = 0; i < b.length; i++){
-    var newTen = [];
-    var result = null;
-    newTen.push(b[i]);
-    console.log("newTen set of nums " + newTen);
-    result = b[i];
-    console.log("Initial result set at " + i + " is " + result);
-    for (var j = i+1; j < b.length; j++){
-      if (result === 10){
-        holdTen.push(newTen);
-        break;
-      }
-      if ((result += b[j]) > 10){
-        j++
-      }
-      result += b[j];
-      newTen.push(b[j]);
+function toTen(strNum) {
+    for (var i = 0; i < b.length; i++) {
+        var newTen = [];
+        var result = b[i];
+        newTen.push(b[i]);
+        for (var j = i+1; j < b.length; ++j) {
+            result += b[j];
+            if (result > 10){
+                result -= b[j];
+            }
+            newTen.push(b[j]);
+            for (var k = j+1; k < b.length; ++k) {
+                if (result < 10){
+                    newTen.push(b[k]);
+                    result += b[k];
+                }
+                if (result > 10){
+                    newTen.pop(b[k]);
+                    result -= b[k];
+                }
+            }
+            if (result === 10){
+                holdTen.push(newTen);
+            }
+        }
     }
-  }
 }
 
 toTen(b);
