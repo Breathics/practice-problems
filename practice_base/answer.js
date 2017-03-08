e0bfba3fb01781dbe63ac40789d4ba8870d874ed
Andy Ong <aong93@gmail.com>
2017-03-08 11:58:54 -0800

    Accounted for 6 whole integers

--- practice_base/answer.js
+++ practice_base/answer.js
@@ -2,9 +2,12 @@ function format_division(x,y){
   var result = x / y;
   var split = result.toString().split('');
   for (var i = (split.indexOf('.') - 3); i >= 0; i-=3){
-    split.splice(i,0,",");
+    if (split[i-1] !== undefined){
+      split.splice(i,0,",");
+    }
   }
   return (split.join(''));
 }
 
-console.log(format_division(123456789123456789,100));
+console.log(format_division(23456789,100));
+console.log(format_division(123456789,100));
